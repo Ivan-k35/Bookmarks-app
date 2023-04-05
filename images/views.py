37b78c00636066bfec3c33015edbe6c578865013
Ivan Koshkin <ivan.koshkin35@gmail.com>
2023-04-05 13:53:08 +0300

    Added a detail view for images

--- images/views.py
+++ images/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.shortcuts import get_object_or_404
+
+from .models import Image
 from .forms import ImageCreateForm
 
 
@@ -24,3 +27,9 @@ def image_create(request):
         form = ImageCreateForm(data=request.GET)
     context = {'section': 'images', 'form': form}
     return render(request, 'images/image/create.html', context)
+
+
+def image_detail(request, pk, slug):
+    image = get_object_or_404(Image, pk=pk, slug=slug)
+    context = {'section': 'images', 'image': image}
+    return render(request, 'images/image/detail.html', context)
