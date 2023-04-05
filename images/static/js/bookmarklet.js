a53bdbd8a71f25aef6045a233f5c26d325a1578e
Ivan Koshkin <ivan.koshkin35@gmail.com>
2023-04-05 11:55:34 +0300

    Updated bookmarklet.js

--- images/static/js/bookmarklet.js
+++ images/static/js/bookmarklet.js
@@ -13,11 +13,12 @@ head.appendChild(link);
 
 // load HTML
 var body = document.getElementsByTagName('body')[0];
-boxHtml = '<div id="bookmarklet">' +
-    '        <a href="#" id="close">$times;</a>' +
-    '        <h1>Select image to bookmark:</h1>' +
-    '        <div class="images"></div>' +
-    '    </div>';
+boxHtml = `
+    <div id="bookmarklet">
+        <a href="#" id="close">&times;</a>
+        <h1>Select an image to bookmark:</h1>
+        <div class="images"></div>
+    </div>`;
 body.innerHTML += boxHtml;
 
 function bookmarkletLaunch() {
@@ -38,12 +39,21 @@ function bookmarkletLaunch() {
     // find images in the DOM with the minimum dimensions
     images = document.querySelectorAll('img[src$=".jpg"], img[src$=".jpeg"], img[src$=".png"]');
     images.forEach(image => {
-        if (image.naturalWidth >= minWidth && image.naturalHeight >= minHeight)
-        {
+        if (image.naturalWidth >= minWidth && image.naturalHeight >= minHeight) {
             var imageFound = document.createElement('img');
             imageFound.src = image.src;
             imagesFound.append(imageFound);
         }
+    })
+
+    // select image event
+    imagesFound.querySelectorAll('img').forEach(image => {
+        image.addEventListener('click', function (event) {
+            imageSelected = event.target;
+            bookmarklet.style.display = 'none';
+            window.open(siteUrl + 'images/create/?url=' + encodeURIComponent(imageSelected.src)
+                + '&title=' + encodeURIComponent(document.title), '_blank');
+        })
     })
 }
 
